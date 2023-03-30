172ac1365e32eaee256f26408d9e533fe2f8b621
ftsnowy <daniil.championat@gmail.com>
2023-03-30 06:56:26 +0300

    added calc

--- lab1/main.py
+++ lab1/main.py
@@ -1,2 +1,27 @@
+import constants
+
+
+def calculate(x, y, op):
+    if op == constants.ADD:
+        return x + y
+    elif op == constants.SUB:
+        return x - y
+    elif op == constants.MUL:
+        return x * y
+    elif op == constants.DIV:
+        if y == 0:
+            return "Can't divide by 0"
+        else:
+            return x / y
+    else:
+        return "Invalid operator"
+
 
 print("HELLO WORLD!")
+print("Enter A:")
+a = int(input())
+print("Enter B:")
+b = int(input())
+print("Enter operator:")
+operator = input()
+print("Result:", calculate(a, b, operator))
